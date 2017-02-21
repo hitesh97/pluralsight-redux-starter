5d10e8000aeba11e01317dcf2230869f7e73d0f0
Hitesh Khatri <khatrih@reedexpo.co.uk>
2017-02-21 09:26:44 +0000

    added redux debug and fixed a bug!

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,23 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, reduxImmutableStateInvariant())
+  // other store enhancers if any
+);
 
 export default function configureStore(initialState) {
     return createStore (
         rootReducer, 
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        enhancer
     );
 }
